868e0aafebfa2171c936c65c82b1ad7b6c26c9bc
NicholasLe04 <Nicholasl.le224@gmail.com>
2023-04-03 11:30:17 -0700

    Restructured Routes

---
+++ server/Controllers/CourseController.js
@@ -0,0 +1,28 @@
+const { getDB } = require("../db/conn");
+
+// Search by course code
+// Takes school (ex: berkeley, deanza), subject (ex: POLI, MATH), and course ID (ex: 14, 15)
+async function getCourse(req, res, next) {
+    const db = await getDB();
+    const courses = await db
+        .collection(req.params.school)
+        .find({
+            "node.courseCodes.courseName": req.params.courseCode.toUpperCase(),
+        })
+        .toArray();
+    res.send(courses);
+}
+
+async function searchByProf(req, res, next) {
+    const db = await getDB();
+    const courses = await db
+        .collection(req.params.school)
+        .find({
+            "node.firstName": req.params.firstName,
+            "node.lastName": req.params.lastName,
+        })
+        .toArray();
+    res.send(courses);
+}
+
+module.exports = { getCourse, getByProf };
