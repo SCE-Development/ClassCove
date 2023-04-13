f8323a924059285157612093a4835d5b2df9354e
arthur <arthur.roth.07@gmail.com>
2023-04-12 21:52:07 -0700

    stashing

--- client/src/Components/Recommendation.js
+++ client/src/Components/Recommendation.js
@@ -1,6 +1,6 @@
 import {useEffect, useState} from "react"; 
 import { useLocation } from 'react-router-dom'
-import { useNavigate } from "react-router-dom";
+import "../stylesheets/login.css";
 
 function Recommendation() { 
     const location = useLocation(); 
@@ -12,16 +12,16 @@ function Recommendation() {
         console.log(location.state);
         setProfessors(location.state.professors); 
         setCourse(location.state.course);
-    }, [])
+    }, [location.state])
 
     function professorRecs() { 
-        if (professors != []) 
+        if (professors !== []) 
             return professors.map(prof => (
                 <div> 
                     <p key={prof.firstName + " " + prof.lastName}>{prof.firstName + " " + prof.lastName}</p>
                     <p key={`${prof.firstName + " " + prof.lastName + "rating"}`}>Rating: {prof.avgRating}</p>
                     <p key={`${prof.firstName + " " + prof.lastName + "wouldTakeAgain"}`}>
-                        Would take again: {prof.wouldTakeAgainPercent} 
+                        Would take again: {Math.max(prof.wouldTakeAgainPercent, 0)}% 
                     </p>
                 </div>
             ))
